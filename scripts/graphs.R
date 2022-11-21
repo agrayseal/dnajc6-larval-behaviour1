218f14954629e5047e7addc30f2ae949857c0d0c
agraceal <gella.gracea@gmail.com>
2022-11-21 16:05:31 +1030

    changes

--- scripts/graphs.R
+++ scripts/graphs.R
@@ -21,16 +21,45 @@ plot_raw <- function(data, col) {
     ggtitle(deparse(substitute(data)))
 }
 
-boxplot_data <- function(data, y) {
-  ggplot(data, aes_string(x = "Genotype", y = y, label = "fish_id")) +
+boxplot_data <- function(data, var) {
+  ggplot(data, aes_string(x = "Genotype", y = var, label = "fish_id")) +
     geom_dotplot(aes(fill = Genotype), 
                   alpha = 0.25, binaxis = "y",
-                  dotsize = 1, stackdir = "center") +
+                  dotsize = 0.6, stackdir = "center") +
     # geom_jitter(aes(colour = get(points)), size = 2, alpha = 0.5) +
     # geom_text_repel() +
+    scale_fill_viridis_d() +
     geom_boxplot(aes(fill = Genotype), alpha = 0.25, outlier.shape = NA) +
     ggtitle(deparse(substitute(data))) +
-    facet_wrap(~Family)
+    ggtitle(labels[labels$var == var, "Feature"]) +
+    ylab(paste0(labels[labels$var == var, "Feature"],
+               " (", labels[labels$var == var, "stat"], " ",
+               labels[labels$var == var, "units"], "/minute)"))
+}
+
+plot_cdot <- function(dat, var) {
+  x <- dat %>%
+    group_by(Bin, Genotype) %>%
+    summarise(mean = mean(get(var)) %>% signif(2), max = max(get(var))) %>%
+    ungroup()
+  
+  p <- tibble(Bin = 1:15, wt.het = 0, wt.hom = 0, het.hom = 0)
+  for (i in 1:15) {
+    y <- aov(get(var) ~ Genotype, data = dat[dat$Bin == i,]) %>% TukeyHSD()
+    p$wt.het[i] <- y[[1]][1,4]
+    p$wt.hom[i] <- y[[1]][2,4]
+    p$het.hom[i] <- y[[1]][3,4]
+  }
+  p$diff <- rowSums(p[,-1] < 0.05) > 0
+  diff <- filter(x, Bin %in% subset(p, diff == T)$Bin)
+  
+  ggplot(x, aes(mean, Bin)) +
+    geom_line(aes(group = Bin), alpha = 0.3) +
+    geom_point(aes(color = Genotype), size = 1.5, alpha = 0.3) +
+    geom_line(data = diff, aes(group = Bin)) +
+    geom_point(data = diff, aes(color = Genotype), size = 2) +
+    geom_text_repel(data = diff, aes(color = Genotype, label = round(mean, 0)), size = 3) +
+    ggtitle(labels[labels$var == var, "Feature"])
 }
 
 # boxplot_data <- function(data, x, y, p = NULL, title = "Title") {
